c0d8db5751c4777247635a3816104325ddd31a88
talmidhon <talmidhon@gmail.com>
2026-04-10 04:34:35 +0300

    Implement ONLYOFFICE restrictions in hermetic-lock.sh
    Added restrictions for ONLYOFFICE, including CSS modifications and removal of the presentation editor directory.

--- files/scripts/hermetic-lock.sh
+++ files/scripts/hermetic-lock.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 set -euo pipefail
-
 echo "Executing Hermetic Lockdown..."
-
-# 1. חסימת שירותים (Masking)
 systemctl mask NetworkManager
 systemctl mask wpa_supplicant
 systemctl mask packagekit.service
 systemctl mask packagekit-offline-update.service
-
-# 2. הסרת מודולי קרנל של רשת (כדי שלא יהיו דרייברים)
-# אנחנו יוצרים קובץ Blacklist שמונע טעינת דרייברים של Wi-Fi ו-Ethernet
 cat <<EOF > /etc/modprobe.d/no-net.conf
 blacklist iwlwifi
 blacklist rtw88_8822ce
@@ -18,33 +12,17 @@ blacklist ath10k_pci
 blacklist e1000e
 blacklist r8169
 EOF
-
-# 3. מחיקת בינאריים רגישים (ליתר ביטחון)
 rm -f /usr/bin/nmcli # כלי השליטה ברשת
 rm -f /usr/bin/nmtui
-
-# 4. נעילת Root מוחלטת
 passwd -l root
-# ==========================================
-# ביטול יכולות Sudo למשתמשים
-# ==========================================
-
-# 1. יצירת חוק שדורס את כל ההרשאות ומונע מכל משתמש להריץ כל פקודה כ-Sudo
 echo "ALL ALL=(ALL) !ALL" > /etc/sudoers.d/99-block-sudo
 chmod 0440 /etc/sudoers.d/99-block-sudo
-
-# 2. הסרת הרשאת ה-SUID מהקובץ הבינארי של Sudo (מונע מהתוכנה את היכולת הטכנית לשנות הרשאות)
 chmod a-s /usr/bin/sudo
 chmod a-s /usr/bin/su
-
-# 1. Restrict permission management to ROOT ONLY (prevents users from making files executable)
-# chmod 700 /usr/bin/chmod
-# chmod 700 /usr/bin/setfacl
-
-# 2. Restrict Flatpak management tools to ROOT ONLY (keeps the engine running for the OS)
 chmod 700 /usr/bin/flatpak
-
-# 3. Restrict critical system manipulation tools to ROOT ONLY
 chmod 700 /usr/bin/rpm-ostree
 chmod 700 /usr/bin/su
-
+echo "Applying ONLYOFFICE restrictions..."
+echo '<style>.document-creation-item[data-id="slide"], a.nav-item[data-value="Presentations"], #idx-sidebar-portals, li:has(a[action="external-panel-0"]), li:has(a[action="settings"]) { display: none !important; }</style>' | tee -a /opt/onlyoffice/desktopeditors/index.html
+rm -rf /opt/onlyoffice/desktopeditors/editors/web-apps/apps/presentationeditor
+echo "ONLYOFFICE restrictions applied successfully."
